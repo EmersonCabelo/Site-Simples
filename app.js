7664f27c362b1605632980d944b321ce83fd5e3c
Emerson Cabelo <emersonmendes2109@hotmail.com>
2025-03-07 15:05:31 -0300

    update 0703

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict'
+
+
+const switcher = document.queryselector('.btn');
+
+switcher.addEventlistener('click', function() { 
+    document.body.classList.toggle('dark-theme')
+
+
+var classnane = document.body.className;
+if(classnane == "light-theme") {
+    this.textcontent = "dark";
+}
+else { this.textcontent = "light";
+
+}
+});
+ console.log('current class name: ')
